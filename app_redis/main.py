f76fdab2cd92642f6f3783c8dd5ccde4ba3ccf8e
ketan purohit <ketanpurohit0@gmail.com>
2022-09-04 11:49:24 +0100

    Redis interaction from fastapi

--- app_redis/main.py
+++ app_redis/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from redis import Redis
 from fastapi import FastAPI
 
@@ -8,8 +9,10 @@ redis = Redis(host='redis', port=6379)
 @app.get("/")
 def read_root():
     redis.incr('hits')
+    redis.set('last', str(datetime.now()))
     counter = str(redis.get('hits'),'utf-8')
-    return f"This webpage has been viewed {counter} time(s)"
+    #redis.bgsave()
+    return f"This *webpage has been viewed {counter} time(s), most recently {redis.get('last')}"
 
 
 
