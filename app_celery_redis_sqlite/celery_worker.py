2ccb034c8755bb4592153ed66c06f7098c0b6762
ketan purohit <ketanpurohit0@gmail.com>
2022-09-04 16:43:28 +0100

    Got working with sqlite. db+ in the backend implies the folder. Creating folder db has 'fixed' - investigate below Test (db missing, sqlalchemy missing)

--- app_celery_redis_sqlite/celery_worker.py
+++ app_celery_redis_sqlite/celery_worker.py
@@ -6,7 +6,7 @@ from celery import Celery
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379" ) # "db+sqlite:///celery.sqlite")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "db+sqlite:///celery.sqlite" ) # "db+sqlite:///celery.sqlite")
 
 
 @celery.task(name="create_task")
