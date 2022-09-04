0682b029652caddcc67bf3b17c2eab7ddf07646c
ketan purohit <ketanpurohit0@gmail.com>
2022-09-04 14:47:16 +0100

    Added celery/redis to FastApi

--- app_celery_redis_sqlite/celery_worker.py
+++ app_celery_redis_sqlite/celery_worker.py
@@ -6,10 +6,10 @@ from celery import Celery
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "db+sqlite:///celery.sqlite")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379" ) # "db+sqlite:///celery.sqlite")
 
 
 @celery.task(name="create_task")
 def create_task(task_type):
-    time.sleep(int(task_type) * 10)
+    time.sleep(int(task_type) * 1)
     return True
