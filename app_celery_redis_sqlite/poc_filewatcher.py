757cb6c5a1a6f428c0776191dc3dca215c4e02c0
Ketan Purohit <ketanpurohit0@gmail.com>
2022-09-10 10:17:26 +0100

    Pass paths to watch as args

--- app_celery_redis_sqlite/poc_filewatcher.py
+++ app_celery_redis_sqlite/poc_filewatcher.py
@@ -1,6 +1,8 @@
 import asyncio
 from watchfiles import awatch, Change
-import pathlib
+from typing import List
+import argparse
+
 
 def filter_specification(change: Change, path: str):
     """Return True if change is to be included, False if ignored
@@ -13,14 +15,20 @@ def filter_specification(change: Change, path: str):
 
     return change == Change.added and path.endswith(allowed_file_extensions)
 
-async def main():
-    path = pathlib.Path(".", "app_celery_redis_sqlite","source_folder",)
-    print(f"Watching folder {path}")
-    async for changes in awatch(path, watch_filter=filter_specification):
+
+async def main(paths: List[str]):
+    print(f"Watching folder {paths}")
+    async for changes in awatch(*paths, watch_filter=filter_specification):
         for change in changes:
             if change[0] == Change.added:
                 print(f"Processing {change}")
             else:
                 print(f"Ignoring {change}")
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Watch folders for new files")
+    parser.add_argument('-p', '--paths', nargs="+", help="Required list of paths", required=True)
+    args = parser.parse_args()
+    print(args)
+    asyncio.run(main(args.paths))
