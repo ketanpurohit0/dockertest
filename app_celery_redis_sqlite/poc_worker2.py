92db4898bbdc8274e4c2d029b25b7ee780e7b974
ketan purohit <ketanpurohit0@gmail.com>
2022-09-10 12:51:33 +0100

    co-operating workers

--- app_celery_redis_sqlite/poc_worker2.py
+++ app_celery_redis_sqlite/poc_worker2.py
@@ -4,34 +4,31 @@ from random import randint
 import time
 
 from celery import Celery
-from poc_worker2 import split_other, split_pdf
+
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "sqla+sqlite:///celery_broker.sqlite")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "db+sqlite:///celery_result.sqlite")
-celery.conf.task_routes = {'poc_worker1.process_pdf': {"queue": "stage1_Queue"}, "poc_worker1.process_other": {"queue": "stage1_Queue"}}  # noqa: E501
+celery.conf.task_routes = {'poc_worker2.split_pdf': {"queue": "stage2_Queue"}, "poc_worker2.split_other": {"queue": "stage2_Queue"}}  # noqa: E501
 
 
 @celery.task
-def process_pdf(path: str):
+def split_pdf(path: str):
     # demo some acivity that takes some time
     _path = pathlib.Path(path)
     print(_path)
     jitter = randint(0, 3)
     time.sleep(len(_path.name)//2 + jitter)
-    split_pdf.delay(path)
-
     return True
 
 
 @celery.task
-def process_other(path: str):
+def split_other(path: str):
     # demo some acivity that takes some time
     _path = pathlib.Path(path)
     print(_path)
     time.sleep(len(_path.name)//2)
-    split_other.delay(path)
     return True
 
 
-# celery --app=poc_worker1.celery worker --loglevel=info -queues stage1_Queue
+# celery --app=poc_worker2.celery worker --loglevel=info --queues stage2_Queue
