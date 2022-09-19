b9eced1b8cb4f5337fa7c2841f783b6b0047e2c4
Ketan Purohit <ketanpurohit0@gmail.com>
2022-09-19 16:30:19 +0100

    Play with selenium

---
+++ demo_selenium.py
@@ -0,0 +1,19 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from pathlib import Path
+opts = Options()
+opts.headless = True
+opts.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\Application\\chrome.exe"
+assert opts.headless  # Operating in headless mode
+#chrome_driver_path = Path(r'C:\MyInstalled\chromedriver\chromedriver.exe')
+driver = Chrome(ChromeDriverManager().install(), options=opts)
+driver.get('https://duckduckgo.com')
+
+search_form = driver.find_element_by_id('search_form_input_homepage')
+search_form.send_keys('real python')
+search_form.submit()
+results = driver.find_elements_by_class_name('result')
+print(results[0].text)
+driver.close()
+
